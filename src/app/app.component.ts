599d96c0d18c73bf8b8c85997fe7c0f881c508da
uriasvitor <uriasvitor@gmail.com>
2022-05-08 03:25:25 -0300

    Calculadora 80%

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AnguCalculator';
+
+  prop = [0,1,2,3,4,5,6,7,8,9,"+","-","*","/","Calcular"];
+  resultado = ""
 }
