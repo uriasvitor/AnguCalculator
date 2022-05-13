104bad206ce552fbbf9becfa17b04db63476dc22
uriasvitor <uriasvitor@gmail.com>
2022-05-13 10:17:26 -0300

    new comits

--- Front-End/src/app/core/components/app.component.ts
+++ Front-End/src/app/core/components/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, DoCheck, OnInit } from '@angular/core';
-import { Subject, takeUntil } from 'rxjs';
 import { reqService } from '../services/requisition.service';
 
 @Component({
@@ -9,43 +8,43 @@ import { reqService } from '../services/requisition.service';
 })
 
 export class AppComponent implements OnInit,DoCheck{
+  display = '0';
+  values = '';
+  prop = [0,1,2,3,4,5,6,7,8,9,"+","-","*","/"];
 
   constructor(private reqService:reqService){}
   ngDoCheck(): void {
-    console.log(this.getnum)
+    console.log(this.display)
   }
 
-  getnum = ''
-  count: any[]=[];
-
-  destroy$: Subject<boolean> = new Subject<boolean>();
-  ngOnInit(): void {
-    this.getAllUsers()
-    console.log(this.count)
+  getNumber(num:any){
+    if(this.display ==='0'){
+      this.display = num.toString();
+      }else{
+        this.display = `${this.display}${num}`;
+        this.values = this.display
+      }
   }
+  getResult(){
+    this.reqService.getCount().subscribe(res =>{
+      console.log('')
+    })
+  }
+  clearDisplay(){
+    this.display = '0';
+  }
+
+  postNumber(num:any){
+    this.reqService.addCount(num).subscribe(Response => {
+      console.log(num);
+      this.getNumber(0)
+      console.log(this.display)
+    })
+  }
+
+  ngOnInit(): void {}
 
   toggleDarkTheme(): void {
     document.body.classList.toggle('dark-theme');
   }
-
-  prop = [0,1,2,3,4,5,6,7,8,9,"+","-","*","/","Calcular"];
-  resultado = "";
-
-  getAllUsers() {
-    this.reqService.getCount().pipe(takeUntil(this.destroy$)).subscribe((count: any) => {
-        this.count = count;
-    });
-  }
-
-  addNumber(num:any){
-    this.reqService.addCount(num).subscribe(Response => {
-      console.log(num)
-    })
-  }
-
-  getNumber(num:any){
-    this.getnum = num;
-
-  }
-
 }
